485f4ba238941ca7dee3014477196bcb0d8d41b7
羊富学 <yfx@china315net.com>
2025-04-14 13:31:47 +0800

    增加Vue模块

---
+++ src/module/tools/files/Vue模块.js
@@ -0,0 +1,11 @@
+export default {
+  id: 'Vue_Model',
+  name: 'Vue模块',
+  collection: [
+    {
+      icon: 'https://www.vue-admin.cn/logo.png',
+      name: 'vue管理平台集中地',
+      link: 'https://www.vue-admin.cn/'
+    }
+  ]
+}
