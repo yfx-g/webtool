3fed8b435c848f3c6ae4ca9cdf626c8c0fbe2bde
羊富学 <yfx@china315net.com>
2025-07-28 17:32:28 +0800

    增加音乐版块

---
+++ src/module/tools/files/音乐版块.js
@@ -0,0 +1,11 @@
+export default {
+  id: 'Music_Module',
+  name: '音乐版块',
+  collection: [
+    {
+      icon: '',
+      name: '落雪音乐',
+      link: 'https://github.com/lyswhut/lx-music-desktop'
+    }
+  ]
+}
